89a234e9e3f68fd590bbe08e4817a10423507f29
Denzel-premium <jindagi2025@gmail.com>
2026-04-19 07:13:18 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,25 +2,36 @@ from pymongo import MongoClient
 from config import MONGO_URL
 
 client = MongoClient(MONGO_URL)
-db = client["telegram_bot"]
+db = client["premium_bot"]
 
 config_col = db["config"]
 folder_col = db["folders"]
 
-# -------- CONFIG --------
+# ---------- CONFIG ----------
 def set_config(key, value):
-    config_col.update_one({"key": key}, {"$set": {"value": value}}, upsert=True)
+    config_col.update_one(
+        {"key": key},
+        {"$set": {"value": value}},
+        upsert=True
+    )
 
 def get_config(key, default=None):
     data = config_col.find_one({"key": key})
     return data["value"] if data else default
 
-# -------- FOLDER --------
+# ---------- FOLDERS ----------
 def add_folder(name):
-    folder_col.update_one({"name": name}, {"$set": {"videos": []}}, upsert=True)
+    folder_col.update_one(
+        {"name": name},
+        {"$set": {"videos": []}},
+        upsert=True
+    )
 
 def add_video(folder, file_id):
-    folder_col.update_one({"name": folder}, {"$push": {"videos": file_id}})
+    folder_col.update_one(
+        {"name": folder},
+        {"$push": {"videos": file_id}}
+    )
 
 def get_folders():
     return list(folder_col.find())
