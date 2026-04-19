3050ad006c84dde054540814198629f8e12d4671
Denzel-premium <jindagi2025@gmail.com>
2026-04-20 03:21:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 
+# ================= TELEGRAM =================
 TOKEN = os.getenv("TOKEN")
-if TOKEN:
-    TOKEN = TOKEN.strip()
 
 ADMIN_ID = os.getenv("ADMIN_ID")
-ADMIN_ID = int(ADMIN_ID) if ADMIN_ID and ADMIN_ID.isdigit() else 0
+ADMIN_ID = int(ADMIN_ID) if ADMIN_ID else None
+
+# ================= MONGO =================
+MONGO_URL = os.getenv("MONGO_URL")
+
+# ================= DEFAULT SETTINGS =================
+DEFAULT_START = "👋 Welcome to Premium Bot"
+DEFAULT_PRICE = "29"
