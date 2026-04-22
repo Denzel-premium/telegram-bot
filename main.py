30b67955b2229930f75ba54f31bea68fd4b60c66
Denzel-premium <jindagi2025@gmail.com>
2026-04-23 00:44:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def expiry_worker():
                     except:
                         pass
 
-                delete_expiry(item["user_id"])
+                delete_expiry(item["_id"])
 
         except Exception as e:
             print("Expiry error:", e)
